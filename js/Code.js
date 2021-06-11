45671102214400217bb8c8f7e1834d165e0514f4
ArjunSingh-YEET <76562412+ArjunSingh-YEET@users.noreply.github.com>
2021-06-11 18:22:08 +0300

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,17 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    fill("white")
+    textSize(15)
+    text("I O T U F C N ",700,150)
+    fill("lightBlue")
+    text("Hint: Performs a specific task !!",700,170)
+}
